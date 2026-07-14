d70da6504e1af2826219127801c46a3473cb84fc
kglapin <kamilglapinski.poczta@gmail.com>
2026-07-14 08:45:30 +0200

    Add structured site data for redesign

---
+++ src/data/site.ts
@@ -0,0 +1,12 @@
+export const site = {
+  name: 'Ubezpieczenia Zimmermann',
+  shortName: 'Zimmermann',
+  description:
+    'Rodzinna multiagencja ubezpieczeniowa w Kaliszu i Krotoszynie. Porównujemy oferty ponad 40 towarzystw i pomagamy także po zakupie polisy.',
+  phonePrimary: '789 315 400',
+  phonePrimaryHref: '+48789315400',
+  phoneRafal: '502 252 048',
+  phoneRafalHref: '+48502252048',
+  phoneIgor: '506 092 200',
+  phoneIgorHref: '+48506092200',
+  facebook: 'https://
